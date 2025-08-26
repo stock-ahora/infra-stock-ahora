fe006001f53900f5c10feddc441b273d8b9730c3
willians briones <wbriones>
2025-08-25 22:31:05 -0400

    ingreso de actualizacion en los contenedores y los servicios de api-gw

---
+++ infra/modules/api-gw.tf
@@ -0,0 +1,13 @@
+module "client-api" {
+  source = "../submodules/api-gw/client"
+  name   = "client-api"
+  aws_lb_nlb_arn = module.ecs.aws_lb_nlb_arn
+  api_ports = {
+    movement     = 8081
+    stock        = 8082
+    client       = 8083
+    notification = 8084
+  }
+  dns_name = module.ecs.dns_name
+  region   = var.aws_region
+}
--- infra/modules/ecs.tf
+++ infra/modules/ecs.tf
@@ -1,14 +1,26 @@
 
-//module "ecs" {
-//  source              = "./modules/ecs"
-//  project_name        = var.project_name
-//  vpc_id              = module.vpc.vpc_id
-//  public_subnet_ids   = module.vpc.public_subnet_ids
-//  instance_type       = var.instance_type
-//  app_image           = var.app_image
-//  app_container_name  = var.app_container_name
-//  app_port            = var.app_port
-//  desired_count       = var.desired_count
-//  allow_cidr_ingress  = var.allow_cidr_ingress
-//  tags                = var.tags
-//}
+module "ecs" {
+  source              = "../submodules/ecs/ecs-go-services"
+
+  region = var.aws_region
+  name = var.project_name
+
+  vpc_id = module.vpc.vpc_id
+  private_app_subnet_ids = module.vpc.private_app_subnet_ids
+  private_app_subnet_cidrs = module.vpc.private_app_subnet_cidrs
+  instance_type_od = ["t4g.micro", "t4g.small"]
+  instance_type_spot = ["t4g.micro", "t4g.small"]
+  ecs_ami_id = ""
+
+  movement_image = "nginx:latest"
+  movement_port = 8081
+
+  stock_image = "nginx:latest"
+  stock_port = 8082
+
+  client_image = "nginx:latest"
+  client_port = 8083
+
+  notification_image = "nginx:latest"
+  notification_port = 8084
+}
